a8f564e8e2fe6d017b844462491ab70b76e4e7b8
maikindasgameplays <143224497+maikindasgameplays@users.noreply.github.com>
2024-10-04 09:38:04 -0300

    s

---
+++ main.js
@@ -0,0 +1,41 @@
+// alert ('voce clicou no botao');                                                                                                                
+// document.querySelector('button');
+// const botao= document.querySelector('button');
+// botao.onclick= function(){
+//   alert('voce clicou no botao');
+// };
+// function soma(num1, num2){
+//   return num1+num2;
+// }
+// console.log(soma(5,3));
+
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(5));
+
+// let valormedia= parseint(prompt(insira o valor 2);
+// let valor= parseint(prompt(insira o valor 3);
+// let medial= parseint(prompt(insira o valor 4);
+
+// let media= function(num2, num3, num4);
+
+// let estados=['PR','SC','RS','MG','RJ','BA','MS','AM','RR','RO','TO','PI','SE','RN','PE','PB','MT','MS','MA','GO','ES','CE','BSB','AP','AL','AC','SP']
+// // let i =0;
+// // while(i <estados.length){
+// // console.log(i);
+// // console.log (estados[i]);
+// // i++;
+// // }
+// for (let i = 0; i < estados.length; i++) {
+//     console.log(i);
+//      console.log (estados[i]);
+
+// }
+let idade=parseint(prompt(Insira a sua idade bb);
+if( idade >17       ){
+console.log('pode dirigir');
+}
+else{
+console.log('ilegal dirigir');
+}
